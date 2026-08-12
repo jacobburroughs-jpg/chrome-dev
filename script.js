97b3fd3bf55ed5a15c6ffffd001deb64d323e26d
Jacob Burroughs <jacobburroughs@Jacobs-MacBook-Air.local>
2026-08-12 00:09:00 -0400

    Expanded load user feature to include prompt and function invocation.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const button = document.querySelector("#load-user");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function() {
 
-    let name = "Jacob";
-    let role = "Developer";
-
-    console.log(name);
-    console.log(role);
+    let user = createUser();
+    
+    console.log(user);
 
 });
+
+function createUser() {
+
+    let name = prompt("What is Your Name?");
+    let role = prompt("What is Your Role?");
+
+    return{
+        name: name,
+        role: role
+    }
+}
