985ad01594b2577cb042fa00e1c998f36e297234
Jacob Burroughs <jacobburroughs@Jacobs-MacBook-Air.local>
2026-08-11 21:53:54 -0400

    Added script.js to expand functionality of site

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("#load-user");
+
+button.addEventListener("click", function () {
+
+    let name = "Jacob";
+    let role = "Developer";
+
+    console.log(name);
+    console.log(role);
+
+});
